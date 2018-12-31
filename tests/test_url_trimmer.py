e348612fc8951c407d8ef7373c0c535782bcdb26
Forest Belton <forest@homolo.gy>
2018-12-31 15:53:28 -0800

    Initial commit

---
+++ tests/test_url_trimmer.py
@@ -0,0 +1,23 @@
+import pytest
+from remove_tracking import ClipboardThread
+
+@pytest.mark.parametrize('before,stripped', [
+    ('https://open.spotify.com/track/1PLEtjblh2EHJlVVIDB84O?si=TQEmHpn1Vf2kNvdcRT87Ng', 'https://open.spotify.com/track/1PLEtjblh2EHJlVVIDB84O'),
+    ('https://www.amazon.com/Set-Theory-Cambridge-Mathematical-Textbooks/dp/1107120322/ref=sr_1_2?ie=UTF8&qid=1544134808&sr=8-2&keywords=set+theory', 'https://www.amazon.com/Set-Theory-Cambridge-Mathematical-Textbooks/dp/1107120322/')
+])
+def test_remove_tracking__matches_url_pattern(before, stripped):
+    exact = ClipboardThread.remove_tracking(before)
+    assert exact == stripped
+
+    suffix = ClipboardThread.remove_tracking(f'A {before}')
+    assert suffix == f'A {stripped}'
+
+    prefix = ClipboardThread.remove_tracking(f'{before} B')
+    assert prefix == f'{stripped} B'
+
+    middle = ClipboardThread.remove_tracking(f'A {before} B')
+    assert middle == f'A {stripped} B'
+
+def test_remove_tracking__no_match():
+    orig = 'there is no match here'
+    assert orig == ClipboardThread.remove_tracking(orig)
---
+++ url_trimmer/url_trimmer.py
@@ -0,0 +1,70 @@
+import abc
+import re
+import sys
+import threading
+import time
+import win32clipboard
+
+from functools import reduce
+
+
+class ClipboardThread(threading.Thread):
+    SLEEP_INTERVAL = 0.1
+    URL_FILTERS = [
+        r'(https?://open.spotify.com/track/.+?)(\?[^\s]+)',
+        r'(https?://www.amazon.com/.*?/dp/.*?/)(ref=[^\s]+)?'
+    ]
+
+    def __init__(self, clipboard):
+        self.__clipboard = clipboard
+
+    def run(self):
+        while True:
+            text = self.__clipboard.get()
+
+            if len(text) != 0:
+                pruned = ClipboardThread.remove_tracking(text)
+                if pruned != text:
+                    self.__clipboard.set(pruned)
+                    print(f'Pruned "{text}" to "{pruned}"', flush=True)
+
+            time.sleep(ClipboardThread.SLEEP_INTERVAL)
+
+    @staticmethod
+    def remove_tracking(text):
+        return reduce(lambda t, f: re.sub(f, r'\1', t), ClipboardThread.URL_FILTERS, text)
+
+
+class TextClipboard(abc.ABC):
+    @abc.abstractmethod
+    def get(self):
+        pass
+
+    @abc.abstractmethod
+    def set(self, text):
+        pass
+
+
+class WindowsTextClipboard(TextClipboard):
+    def get(self):
+        text = ''
+
+        try:
+            win32clipboard.OpenClipboard()
+            text = win32clipboard.GetClipboardData()
+            win32clipboard.CloseClipboard()
+        except Exception as err:
+            print(f'failed to retrieve clipboard data: {type(err)} {err}', file=sys.stderr)
+
+        return text
+
+    def set(self, text):
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardText(text)
+        win32clipboard.CloseClipboard()
+
+
+if __name__ == '__main__':
+    clipboard = WindowsTextClipboard()
+    ClipboardThread(clipboard).run()
