5d2323ae0a7c8e3e4fb9835fb8340e745dbb9a37
Forest Belton <forest@homolo.gy>
2019-01-01 19:05:49 -0800

    Add system tray icon

--- url_trimmer.py
+++ url_trimmer.py
@@ -1,7 +1,11 @@
+from pystray import Icon, Menu, MenuItem
+from PIL import Image, ImageDraw
+
 from url_trimmer.clipboard_thread import ClipboardThread
 from url_trimmer.clipboard.windows_text_clipboard import WindowsTextClipboard
 
 if __name__ == '__main__':
+    # Set up the clipboard thread
     filters = [
         r'(https?://open.spotify.com/track/.+?)(\?[^\s]+)',
         r'(https?://www.amazon.com/.*?/dp/.*?/)(ref=[^\s]+)?'
@@ -9,5 +13,13 @@ if __name__ == '__main__':
 
     clipboard = WindowsTextClipboard()
     thread = ClipboardThread(clipboard, filters)
+    thread.start()
 
-    thread.run()
+    # Start the system tray icon
+    image = Image.open('icon.png')
+    icon = Icon('URL Trimmer', icon=image, menu=Menu(
+        MenuItem('Settings', lambda icon, item: None),
+        MenuItem('Exit', lambda icon, item: icon.stop())
+    ))
+
+    icon.run()
